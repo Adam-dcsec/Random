f92be536683fd46eef6bc49d7999b684064dfa44
Adam-dcsec <adaaam1337@gmail.com>
2020-11-02 15:35:42 -0500

    Update socketwebserver.py

--- socketwebserver.py
+++ socketwebserver.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python3
 
 import socket
-import sys
 import os
-import glob
-import re
  
 
 HOST = '' 
 PORT = 8080 
 #path = '/root/Desktop/Scripts/testpad'
 
-test = 'index.html'
+#test = 'index.html'
 
-def walk(fileToBeServed):
-#def walk():
+#Function that dirwalks recursively and endings content based on if file was found in webroot or not
+def walkSend(fileToBeServed):
+    found = False
     os.chdir("/var/www/html")
     for root, dirs, files in os.walk(".", topdown = False):
        for name in files:
-          files = os.path.join(root, name)
-          matchedFile = re.findall(r"./" + fileToBeServed, files, flags=re.IGNORECASE)
-          if matchedFile:
-               for i in matchedFile:
-                    print(i)
-          #print(files)
-   #for name in dirs:
-      #print(os.path.join(root, name))
-      
-walk(test)
+          #print(name)
+          if os.path.join(root, name) == '.' + fileToBeServed:
+          #File has been found so serving it up
+              print('it works')
+              found = True 
+              fileserved = open('/var/www/html' + fileToBeServed, 'r')
+              content = fileserved.read()
+              conn.sendall(b'HTTP/1.1 200 OK\n')
+              conn.sendall(b'Server: test\n')
+              conn.sendall(b'Content-Type: text/html\n')
+              conn.sendall(b'\n')
+              conn.sendall(content.encode())   
+          else:
+              continue
+    if not found:
+    #Sending 404 Page because resource was not found
+        conn.sendall(b'HTTP/1.1 404 OK\n')
+        conn.sendall(b'Server: test\n')
+        conn.sendall(b'Content-Type: text/html\n')
+        conn.sendall(b'\n')
+        conn.sendall(b'no dawg')
+        print("404")
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -41,39 +51,19 @@ print('Socket bind complete')
 
 #print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
-#directory = os.listdir()
-#print(directory)
-
-#fileserved = open(path, 'r')
-#content = fileserved.read()
-#print(content.split()[1])
-
-
-
-
 while True:
     #After every closed connection you need to relisten and reaccept any connection but you DONT NEED TO RELISTEN ON YOUR PORT OR YOU WILL GET ADDRESS IN USE ERRORS REEEEEEEE
     serversocket.listen(10)
     conn, addr = serversocket.accept()
       
     #Assigning data to the connection recieved
-    #data = conn.recv(1024)
-    #data
-    #conn.recv(1024)
     s = conn.recv(1024)
+    
+    #Finding the requested content to serve
     ss = s.split()[1]
-    serveMe = '.' + ss.decode("utf-8")
+    serveMe = ss.decode("utf-8")
     print(serveMe)
     
-    conn.sendall(b'HTTP/1.1 200 OK\n')
-    conn.sendall(b'Server: test\n')
-    conn.sendall(b'Content-Type: text/html\n')
-    conn.sendall(b'\n')
-    #conn.sendall(ss.encode())
-    #conn.sendall(b'HTTP/1.1 200 OK\n')
-    #conn.sendall(b'Server: test\n')
-    #conn.sendall(b'Content-Type: text/html\n')
-    #conn.sendall(b'\n')
-    #Sending back the data that was recieved
-    #conn.sendall(content.encode())
+    #Running function to serve content
+    walkSend(serveMe)
     conn.close()
