57450448eaa0e92bce5896b648546e5c7ac618d0
Adam-dcsec <adaaam1337@gmail.com>
2020-11-02 13:12:09 -0500

    Update socketwebserver.py

--- socketwebserver.py
+++ socketwebserver.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
-#InProgress
 
 import socket
 import sys
+import os
+import glob
+import re
  
+
 HOST = '' 
 PORT = 8080 
+#path = '/root/Desktop/Scripts/testpad'
+
+test = 'index.html'
+
+def walk(fileToBeServed):
+#def walk():
+    os.chdir("/var/www/html")
+    for root, dirs, files in os.walk(".", topdown = False):
+       for name in files:
+          files = os.path.join(root, name)
+          matchedFile = re.findall(r"./" + fileToBeServed, files, flags=re.IGNORECASE)
+          if matchedFile:
+               for i in matchedFile:
+                    print(i)
+          #print(files)
+   #for name in dirs:
+      #print(os.path.join(root, name))
+      
+walk(test)
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,18 +41,39 @@ print('Socket bind complete')
 
 #print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
+#directory = os.listdir()
+#print(directory)
+
+#fileserved = open(path, 'r')
+#content = fileserved.read()
+#print(content.split()[1])
+
+
+
+
 while True:
     #After every closed connection you need to relisten and reaccept any connection but you DONT NEED TO RELISTEN ON YOUR PORT OR YOU WILL GET ADDRESS IN USE ERRORS REEEEEEEE
     serversocket.listen(10)
     conn, addr = serversocket.accept()
+      
     #Assigning data to the connection recieved
-    data = conn.recv(1024)
+    #data = conn.recv(1024)
+    #data
+    #conn.recv(1024)
+    s = conn.recv(1024)
+    ss = s.split()[1]
+    serveMe = '.' + ss.decode("utf-8")
+    print(serveMe)
+    
+    conn.sendall(b'HTTP/1.1 200 OK\n')
+    conn.sendall(b'Server: test\n')
+    conn.sendall(b'Content-Type: text/html\n')
+    conn.sendall(b'\n')
+    #conn.sendall(ss.encode())
+    #conn.sendall(b'HTTP/1.1 200 OK\n')
+    #conn.sendall(b'Server: test\n')
+    #conn.sendall(b'Content-Type: text/html\n')
+    #conn.sendall(b'\n')
     #Sending back the data that was recieved
-    conn.send(data)
+    #conn.sendall(content.encode())
     conn.close()
-    #serversocket.close()
-    #serversocket.listen(10)
-    if not data:
-        break
-    conn.sendall(data)
-    #serversocket.close()
